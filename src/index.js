1bcddb0e64ab5c4d52ceab929ab2b793d8166b52
Marcelo Nicolas Carrizo <marcelslaam@gmail.com>
2025-08-18 22:40:14 -0300

    Practicas para iniciar la navegacion

--- src/index.js
+++ src/index.js
@@ -2,16 +2,14 @@ import express from 'express'
 import {dirname, join} from 'path'
 import { fileURLToPath } from 'url'
 
+import indexRoutes from './routes/index.js'
 const app = express()
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 app.set('views', join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-
-app.get('/', (req, res) => res.render('index'))
-app.get('/about', (req, res) => res.render('about'))
-app.get('/contact', (req, res) => res.render('contact'))
+app.use(indexRoutes)
 
 app.listen(3000)
 console.log('Server is listening on port', 3000) 
