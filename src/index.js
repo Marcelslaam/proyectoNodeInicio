070e4d58691878585b5b820e15ae8cfe2e5bcd18
Marcelo Nicolas Carrizo <marcelslaam@gmail.com>
2025-08-21 22:25:23 -0300

    Retoques siguiendo el tutorial de node

--- src/index.js
+++ src/index.js
@@ -11,5 +11,7 @@ app.set('views', join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use(indexRoutes)
 
+app.use(express.static(join (__dirname, 'public')))
+
 app.listen(3000)
 console.log('Server is listening on port', 3000) 
